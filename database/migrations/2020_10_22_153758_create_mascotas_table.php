1595bd8a379cab5ad443bdd08f97cda5fb72b608
Luis Rioja <luis.rioja11@gmail.com>
2020-10-22 18:20:24 +0200

    first commit

---
+++ database/migrations/2020_10_22_153758_create_mascotas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMascotasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mascotas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('edad');
+            $table->string('nombre', 20);
+            $table->string('tipo', 20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mascotas');
+    }
+}
